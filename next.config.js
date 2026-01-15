b83970d40dcbdfd3b6e2dedae087c657aacae6e6
brondav5-cloud <brondav5@gmail.com>
2026-01-15 15:49:07 +0200

    v7.0 - הגדרות ספים נפרדים, תיקוני build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
   trailingSlash: true,
-};
-module.exports = nextConfig;
+  images: {
+    unoptimized: true
+  }
+}
+module.exports = nextConfig
